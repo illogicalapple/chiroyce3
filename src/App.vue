94795722b7d0f8d8547ecbe105f0290c875411a6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-04-11 09:51:07 -0500

    Update App.vue
    icons not added yet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <main id="content">
     <img class="logo" alt="Logo" src="./assets/chiroyce.svg" />
+    <div class="bio" or-something>
+      <h1>Chiroyce</h1>
+      <p>lorem ipsum dolor sit amet</p>
+    </div>
   </main>
 </template>
 
@@ -12,11 +16,19 @@
   img.logo {
     position: absolute;
     top: 50%;
-    left: 30vw;
+    left: 30%;
     transform: translateY(-50%);
-    height: 50vh; /* https://illogicalapple.com/blog/css-vh-bugs */
+    width: 30vh; /* https://illogicalapple.com/blog/css-vh-bugs */
+  }
+  div.bio {
+    position: absolute;
+    top: 50%;
+    left: calc(60% + 30vh);
+    transform: translateY(-50%);
+    width: calc(90% - (60% + 30vh));
   }
 </style>
+
 <style>
   main#content {
     width: 100vw;
